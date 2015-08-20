5911c470480fcab710e1e32df16d79af31f21459
NIpuna Cooray <nipuna.cooray@gmail.com>
2015-08-20 23:29:52 +0530

    Test

--- QCODSS/src/com/qcodss/struts/AddPlantsAction.java
+++ QCODSS/src/com/qcodss/struts/AddPlantsAction.java
@@ -1,5 +1,7 @@
 package com.qcodss.struts;
 
+import com.qcodss.dao.PlantDAO;
+
 public class AddPlantsAction {
 	
 	public String name;
@@ -8,7 +10,9 @@ public class AddPlantsAction {
 		
 		String returnVal = "success";
 		
+		
 		System.out.println(name);
+		
 		
 		
 		return returnVal;
